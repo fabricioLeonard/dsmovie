76f0eda497f56e409e7c060bd1ddc9152727aea0
Fabricio Leonard Leopoldino <fabricioleonard@gmail.com>
2022-01-14 21:49:15 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -2,6 +2,8 @@ package com.devsuperior.dsmovie.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_movie")
@@ -14,6 +16,8 @@ public class Movie implements Serializable {
     private Double score;
     private Integer count;
     private String image;
+    @OneToMany(mappedBy = "id.movie")
+    private Set<Score> scores = new HashSet<>();
 
     public Movie() {
     }
@@ -66,5 +70,7 @@ public class Movie implements Serializable {
         this.image = image;
     }
 
-
+    public Set<Score> getScores() {
+        return scores;
+    }
 }
